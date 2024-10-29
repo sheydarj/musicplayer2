e7fee4ef0e578c36574d1a3f83a632f655a332ef
farnazdarvish <f.darvish1996@gmail.com>
2024-10-29 22:56:20 +0330

    add audio player

--- js/index.js
+++ js/index.js
@@ -1,5 +1,53 @@
 const menu=document.getElementById('hamburger')
-let mod=1
+const ul=document.getElementById('ul')
+const pic1=document.getElementById('pic1')
+const play=document.getElementById('play')
+const icon=document.getElementById('icon')
+const pic=document.getElementById('pic')
+const music=document.getElementById('music')
+const h2=document.querySelector('#music>h2')
+const txt=document.getElementById('txt')
+const audio=document.getElementById('audio')
+const playBtn=document.getElementById('playBtn')
+
+let mod = true;
+audio.play()
+
 menu.addEventListener('click' , ()=>{
-    menu.classList.toggle('text-red')
+
+    if(mod){
+        menu.style.color='red';
+        pic.style.height='150px'
+        music.style.top='15%'
+        h2.style.color='black'
+        txt.style.height='0px'
+
+
+
+        mod = false
+    }else{
+        menu.style.color='black';
+        pic.style.height='280px'
+        music.style.top=''
+        h2.style.color='white'
+        txt.style.height='200px'
+
+
+
+        mod = true
+    }
+    ul.classList.toggle('hidden')
+    pic1.classList.toggle('hidden')
+    play.classList.toggle('hidden')
+    icon.classList.toggle('hidden')
+
+
+
 })
+// playBtn.addEventListener('click', () => {
+//     audio.setAttribute('controls')
+//     // if (audio.paused) {
+//     //     audio.play()
+//     // }
+// })
+// 
