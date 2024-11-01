c5fd5abd8b8ebb482cf0b38c04d3a5be27a1d195
Sheyda Rajaee <sheyda.rajaee123@gmail.com>
2024-11-01 21:47:04 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,42 +1,40 @@
-const menu = document.getElementById('hamburger')
-const ul = document.getElementById('ul')
-const pic1 = document.getElementById('pic1')
-const play = document.getElementById('play')
-const icon = document.getElementById('icon')
-const pic = document.getElementById('pic')
-const music = document.getElementById('music')
-const h2 = document.querySelector('#music>h2')
-const txt = document.getElementById('txt')
+const menu = document.getElementById("hamburger");
+const ul = document.getElementById("ul");
+const pic1 = document.getElementById("pic1");
+const play = document.getElementById("play");
+const icon = document.getElementById("icon");
+const pic = document.getElementById("pic");
+const music = document.getElementById("music");
+const h2 = document.querySelector("#music>h2");
+const txt = document.getElementById("txt");
 // const audio = document.getElementById('audio')
-const timer = document.getElementById('playing-time')
-const duration = document.getElementById('music-duration')
+const timer = document.getElementById("playing-time");
+const duration = document.getElementById("music-duration");
 
 let mod = true;
+let playing = false;
 
-menu.addEventListener('click', () => {
-
-    if (mod) {
-        menu.style.color = 'red';
-        pic.style.height = '150px'
-        music.style.top = '15%'
-        h2.style.color = 'white'
-        // txt.style.height = '0px'
-        mod = false
-    } else {
-        menu.style.color = 'black';
-        pic.style.height = '280px'
-        music.style.top = ''
-        h2.style.color = 'white'
-        // txt.style.height = '200px'
-        mod = true
-    }
-    ul.classList.toggle('hidden')
-    pic1.classList.toggle('hidden')
-    play.classList.toggle('hidden')
-    icon.classList.toggle('hidden')
-
-})
-
+menu.addEventListener("click", () => {
+  if (mod) {
+    menu.style.color = "red";
+    pic.style.height = "150px";
+    music.style.top = "15%";
+    h2.style.color = "white";
+    // txt.style.height = '0px'
+    mod = false;
+  } else {
+    menu.style.color = "black";
+    pic.style.height = "280px";
+    music.style.top = "";
+    h2.style.color = "white";
+    // txt.style.height = '200px'
+    mod = true;
+  }
+  ul.classList.toggle("hidden");
+  pic1.classList.toggle("hidden");
+  play.classList.toggle("hidden");
+  icon.classList.toggle("hidden");
+});
 
 //  first way ///////////////////////////////////////////////////////////////////////////////
 
@@ -88,8 +86,8 @@ menu.addEventListener('click', () => {
 // end of first way ///////////////////////////////////////////////////////////////////////////////
 
 // second way ///////////////////////////////////////////////////////////////////////////////
-let audios = document.querySelectorAll('audio');
-const audioList = document.querySelectorAll('#ul>li')
+let audios = document.querySelectorAll("audio");
+const audioList = document.querySelectorAll("#ul>li");
 // audioList.forEach((li, index) => {
 //     li.addEventListener('click', (e) => {
 //         if (audios[index].paused) {
@@ -104,161 +102,152 @@ const audioList = document.querySelectorAll('#ul>li')
 //     })
 // })
 
-
-let playing = false;
-const playBtn = document.getElementById('play-btn');
-const pauseBtn = document.getElementById('pause-btn');
+const playBtn = document.getElementById("play-btn");
+const pauseBtn = document.getElementById("pause-btn");
 // end of second way ///////////////////////////////////////////////////////////////////////////////
 
 // play && pause /////////////////////////////////////
 let musicIndex = 0;
 function playPauseBtn() {
-    if (audios[musicIndex].paused) {
-        startInterval()
+  if (playing) {
+    startInterval();
 
-        audios[musicIndex].play()
-        playBtn.style.display = 'none';
-        pauseBtn.style.display = 'block';
-        document.getElementById('music-name').innerHTML = audios[musicIndex].dataset.name;
-    } else {
-        audios[musicIndex].pause()
-        clearInterval(timerInterval)
-        playBtn.style.display = 'block';
-        pauseBtn.style.display = 'none';
-    }
+    audios[musicIndex].play();
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "block";
+    document.getElementById("music-name").innerHTML =
+      audios[musicIndex].dataset.name;
+    playing = false;
+  } else {
+    audios[musicIndex].pause();
+    clearInterval(timerInterval);
+    playBtn.style.display = "block";
+    pauseBtn.style.display = "none";
+    playing = true;
+  }
 }
 // end of play && pause /////////////////////////////////////
 
-
 // forward && rewind CONTROLLS ///////////////////////////////
-let forwardBtn = document.getElementById('forward');
-let rewindBtn = document.getElementById('rewind');
-forwardBtn.addEventListener('click', () => {
-    // if (playing) {
-    //     audios.forEach((audio) => {
-    //         audio.pause()
-    //     })
-    // }
-    if (musicIndex + 1 < audios.length) {
-        audios[musicIndex].pause()
-        musicIndex++
-        console.log(musicIndex);
-        audios[musicIndex].play()
-        playing = true;
-        document.getElementById('music-name').innerHTML = audios[musicIndex].dataset.name;
-    } else {
-        audios[musicIndex].pause()
-        musicIndex = 0;
-        audios[musicIndex].play()
-        playing = true;
-        document.getElementById('music-name').innerHTML = audios[musicIndex].dataset.name;
-    }
-})
-rewindBtn.addEventListener('click', () => {
-    if (musicIndex < audios.length && musicIndex > 0) {
-        audios[musicIndex].pause()
-        musicIndex--
-        audios[musicIndex].play()
-        playing = true;
-        document.getElementById('music-name').innerHTML = audios[musicIndex].dataset.name;
-    } else {
-        audios[musicIndex].pause()
-        musicIndex = audios.length - 1;
-        audios[musicIndex].play()
-        playing = true;
-        document.getElementById('music-name').innerHTML = audios[musicIndex].dataset.name;
-    }
-})
+let forwardBtn = document.getElementById("forward");
+let rewindBtn = document.getElementById("rewind");
+forwardBtn.addEventListener("click", () => {
+  // if (playing) {
+  //     audios.forEach((audio) => {
+  //         audio.pause()
+  //     })
+  // }
+  audios[musicIndex].pause();
+  if (musicIndex + 1 < audios.length) {
+    musicIndex++;
+  } else {
+    musicIndex = 0;
+  }
+  if (playing) {
+    audios[musicIndex].play();
+  }
+
+  document.getElementById("music-name").innerHTML =
+    audios[musicIndex].dataset.name;
+});
+//end of frwrd
+//start of rewind
+rewindBtn.addEventListener("click", () => {
+  audios[musicIndex].pause();
+  if (musicIndex < audios.length && musicIndex > 0) {
+    musicIndex--;
+  } else {
+    musicIndex = audios.length - 1;
+  }
+
+  if (playing) {
+    audios[musicIndex].play();
+  }
+  document.getElementById("music-name").innerHTML =
+    audios[musicIndex].dataset.name;
+});
 // end of forward && rewind CONTROLLS ///////////////////////////////
 
 let timerInterval;
 audioList.forEach((li, index) => {
-    li.addEventListener('click', (e) => {
-        let audio = document.getElementsByClassName('audio')
-        changeAudioSource(audio.item(index))
-        resetTimer();
-        let audioDuration = audio.item(index).duration
+  li.addEventListener("click", (e) => {
+    let audio = document.getElementsByClassName("audio");
+    changeAudioSource(audio.item(index));
+    resetTimer();
+    let audioDuration = audio.item(index).duration;
 
+    getMinutes(audioDuration);
+    startCounter(audioDuration);
+    // console.log('my audioooooooo', audio.item(index).buffered.start(0.2))
 
-        getMinutes(audioDuration)
-        startCounter(audioDuration)
-        // console.log('my audioooooooo', audio.item(index).buffered.start(0.2))
+    // Timer.innerHTML = audioDuration
 
-        // Timer.innerHTML = audioDuration
-
-        // hide ul list
-        ul.classList.toggle('hidden')
-        play.classList.toggle('hidden')
-        icon.classList.toggle('hidden')
-        // end of hide ul list
-        audios[musicIndex].pause()
-        musicIndex = index;
-        audios[index].play()
-        playing = true;
-        document.getElementById('music-name').innerHTML = audios[musicIndex].dataset.name;
-        // playPauseBtn()
-    })
-})
-
+    // hide ul list
+    ul.classList.toggle("hidden");
+    play.classList.toggle("hidden");
+    icon.classList.toggle("hidden");
+    // end of hide ul list
+    audios[musicIndex].pause();
+    musicIndex = index;
+    if (playing) {
+      audios[index].play();
+    }
+    document.getElementById("music-name").innerHTML =
+      audios[musicIndex].dataset.name;
+    // playPauseBtn()
+  });
+});
 
 function getMinutes(s) {
-    const minutes = Math.floor(s / 60)
-    console.log('minutes ', minutes)
-    const second = Math.floor((((s / 60) - minutes) * 60))
-    console.log('second ', second)
-    duration.innerText = `${minutes.toString()}:${second.toString()}`
-
+  const minutes = Math.floor(s / 60);
+  console.log("minutes ", minutes);
+  const second = Math.floor((s / 60 - minutes) * 60);
+  console.log("second ", second);
+  duration.innerText = `${minutes.toString()}:${second.toString()}`;
 }
 
-
 function startCounter(s) {
+  let secondsElapsed = 0;
+  timer.innerText = "";
+  playBtn.style.display = "none";
+  pauseBtn.style.display = "block";
+  timerInterval = setInterval(() => {
+    secondsElapsed++;
+    const minutes = String(Math.floor(secondsElapsed / 60)).padStart(2, "0");
+    const seconds = String(secondsElapsed % 60).padStart(2, "0");
+    timer.innerText = `${minutes}:${seconds}`;
 
-    let secondsElapsed = 0;
-    timer.innerText = ""
-    playBtn.style.display = 'none';
-    pauseBtn.style.display = 'block';
-    timerInterval = setInterval(() => {
-        secondsElapsed++;
-        const minutes = String(Math.floor(secondsElapsed / 60)).padStart(2, '0');
-        const seconds = String(secondsElapsed % 60).padStart(2, '0');
-        timer.innerText = `${minutes}:${seconds}`;
-
-        // Stop the timer when audio ends
-        if (secondsElapsed >= s) {
-            clearInterval(timerInterval);
-            audio.pause();
-        }
-    }, 1000);
-
-
+    // Stop the timer when audio ends
+    if (secondsElapsed >= s) {
+      clearInterval(timerInterval);
+      audio.pause();
+    }
+  }, 1000);
 }
 
 function startInterval() {
-    timerInterval = setInterval(() => {
-        let current = audios[musicIndex].currentTime.toFixed(0)
-        const minutes = String(Math.floor(current / 60)).padStart(2, '0');
-        const seconds = String(current % 60).padStart(2, '0');
-        timer.innerText = `${minutes}:${seconds}`;
-    }, 1000)
+  timerInterval = setInterval(() => {
+    let current = audios[musicIndex].currentTime.toFixed(0);
+    const minutes = String(Math.floor(current / 60)).padStart(2, "0");
+    const seconds = String(current % 60).padStart(2, "0");
+    timer.innerText = `${minutes}:${seconds}`;
+  }, 1000);
 }
 
 function resetTimer() {
-    clearInterval(timerInterval);
-    timer.innerText = '00:00'; // Reset display
+  clearInterval(timerInterval);
+  timer.innerText = "00:00"; // Reset display
 }
 
 function changeAudioSource(newSource) {
-    newSource.currentTime = 0
-    resetTimer(); // Reset timer for the new audio
+  newSource.currentTime = 0;
+  resetTimer(); // Reset timer for the new audio
 }
-
 
 window.onload = function () {
-    let audio = document.getElementsByClassName('audio')
-    console.log("audioeeeee1111", audio)
-    document.getElementById('music-name').innerHTML = audio.item(0).dataset.name;
-    getMinutes(audio.item(0).duration)
-    // addItem(item.title, item.src, item.id)
-
-
-}
+  let audio = document.getElementsByClassName("audio");
+  console.log("audioeeeee1111", audio);
+  document.getElementById("music-name").innerHTML = audio.item(0).dataset.name;
+  getMinutes(audio.item(0).duration);
+  // addItem(item.title, item.src, item.id)
+};
